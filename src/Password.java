a89a6a099ec183da2a6870e9eab3e5a568e94a16
Carl0s26 <Carloskepp26@gmail.com>
2023-11-02 09:39:50 -0400

    Added Date requirement
    Added Date requirement,
    Fixed +,= symbols
    Fixed length issue adding = to the >

--- src/Password.java
+++ src/Password.java
@@ -1,7 +1,15 @@
 
   // Imports \\
 
-import java.util.Scanner;
+
+
+  import java.util.Scanner;
+
+// time imports
+
+import java.text.Format;  
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
 
 public class Password {
     public static void main(String[] args) {
@@ -9,7 +17,7 @@ public class Password {
     int sum_numbers = 0;
     int number = 0;
 
-    String[] Symbols = {"!", "@","#","$","%","^","&","*","(",")","`","~","{","}","[","]",":",";","'","\"","|","\\ ","/","?",".",">",",","<","-","_"};
+    String[] Symbols = {"!", "@","#","$","%","^","&","*","(",")","`","~","{","}","[","]",":",";","'","\"","|","\\ ","/","?",".",">",",","<","-","_","=","+"};
     String[] countries = {"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda","Palestine", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe", "Palestine"};
     String[] Capital_letters = {"Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","K","L","Z","X ","C","V","B","N","M",};
     String[] Numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -17,6 +25,8 @@ public class Password {
     String[] Lower_letters = {"q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"};
     String user_password = "";
     Scanner Leedor = new Scanner(System.in);
+    Format time_format = new SimpleDateFormat("EEEE");  
+    String Day_name = time_format.format(new Date());  
 
     System.out.println("Please choose a new Password");
 
@@ -32,15 +42,15 @@ public class Password {
           System.out.println(e + " Please try again");
         }
       } 
-      else if (user_password.length() > 10) { // > 10 Continue
-        if (user_password.length() > 30) { // > 30 END
+      else if (user_password.length() >= 10) { // > 10 Continue
+        if (user_password.length() > 35) { // > 30 END
           try {
-            throw new RuntimeException("Your password must not be over 30 characters,");
+            throw new RuntimeException("Your password must not be over 35 characters,");
           } 
           catch (Exception e) {
             System.out.println(e + " Please try again");
           }
-        } else if (user_password.length() < 30) { // < 30 Continue
+        } else if (user_password.length() <= 35) { // <= 35 Continue
           number = 0;
           for (String i : Symbols) {
             if (user_password.contains(i)) {
@@ -102,8 +112,17 @@ public class Password {
                                 }
                               }
                               if (number >= 1) {
-                                System.out.println(" Thats A Great Password");
-                                break;
+                                if (user_password.toLowerCase().contains(Day_name.toLowerCase())) {
+                                    System.out.println(" Thats A Great Password");
+                                    break;
+                                }
+                                else{
+                                    try {
+                                        throw new RuntimeException("Your password must have today's day name,");
+                                    } catch (Exception e) {
+                                        System.out.println(e + " Please try again");
+                                    }
+                                }
                               }
                               else{
                                 try {
@@ -177,7 +196,7 @@ public class Password {
               }
               else { // == 3 Numbers End
                 try {
-                  throw new RuntimeException("Your password must have at least 3 Different numbers, ");
+                  throw new RuntimeException("Your password must have at least 3 numbers, ");
                 } 
                 catch (Exception e) {
                   System.out.println(e + "Please try again");
